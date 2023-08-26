631849f305c9b20f9540e9a98463efbb37ccc8a0
rika.kohda <>
2023-08-26 21:05:40 +0900

    講座の最後まで実施

--- its_springboot/src/main/java/com/example/its/web/issue/IssueController.java
+++ its_springboot/src/main/java/com/example/its/web/issue/IssueController.java
@@ -1,23 +1,50 @@
 package com.example.its.web.issue;
 
 import com.example.its.domain.issue.IssueEntity;
+import com.example.its.domain.issue.IssueService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/issues")
+@RequiredArgsConstructor
 public class IssueController {
 
-    @GetMapping("/issues")
+    private final IssueService issueService;
+
+    @GetMapping
     public String showList(Model model) {
-        var issueList = List.of(
-                new IssueEntity(1,"概要1","説明1"),
-                new IssueEntity(2,"概要2","説明2"),
-                new IssueEntity(3,"概要3","説明3")
-        );
-        model.addAttribute("issueList", issueList);
+        model.addAttribute("issueList", issueService.findAll());
         return "issues/list";
+    }
+
+
+    @GetMapping("/creationForm")
+    public String showCreationForm(Model model) {
+        if (!model.containsAttribute("issueForm")) {
+            model.addAttribute("issueForm", new IssueForm());
+        }
+        return "issues/creationForm";
+    }
+
+    @PostMapping
+    public String create(@Validated @ModelAttribute("issueForm") IssueForm form, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "issues/creationForm";
+        }
+        issueService.create(form.getSummary(), form.getDescription());
+        return "redirect:/issues";
+    }
+
+    @GetMapping("/{issueId}")
+    public String showDetail(@PathVariable("issueId") long issueId, Model model) {
+        model.addAttribute("issue", issueService.findById(issueId));
+        return "issues/detail";
     }
 }
